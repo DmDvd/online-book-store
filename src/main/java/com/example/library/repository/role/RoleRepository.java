ed86fdd36e0e134733953cf4e57091c45d7866f6
Dima Davydov <Kilo6559@gmail.com>
2025-02-06 11:43:35 +0200

    Corrected

--- src/main/java/com/example/library/repository/role/RoleRepository.java
+++ src/main/java/com/example/library/repository/role/RoleRepository.java
@@ -2,10 +2,8 @@ package com.example.library.repository.role;
 
 import com.example.library.model.Role;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    @EntityGraph
     Optional<Role> findByName(Role.RoleName name);
 }
--- src/main/java/com/example/library/repository/user/UserRepository.java
+++ src/main/java/com/example/library/repository/user/UserRepository.java
@@ -2,10 +2,12 @@ package com.example.library.repository.user;
 
 import com.example.library.model.User;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByEmail(String email);
 }
