f9177a40d97107fc42280cd809de5af3844c8fda
Dima Davydov <Kilo6559@gmail.com>
2025-02-10 14:11:12 +0200

    added categoriesÐÐ

---
+++ src/main/java/com/example/library/dto/category/CreateCategoryRequestDto.java
@@ -0,0 +1,12 @@
+package com.example.library.dto.category;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class CreateCategoryRequestDto {
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+}
--- src/main/java/com/example/library/mapper/BookMapper.java
+++ src/main/java/com/example/library/mapper/BookMapper.java
@@ -2,8 +2,12 @@ package com.example.library.mapper;
 
 import com.example.library.config.MapperConfig;
 import com.example.library.dto.book.BookDto;
+import com.example.library.dto.book.BookDtoWithoutCategoryIds;
 import com.example.library.dto.book.CreateBookRequestDto;
 import com.example.library.model.Book;
+import com.example.library.model.Category;
+import java.util.List;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
@@ -14,4 +18,16 @@ public interface BookMapper {
     Book toModel(CreateBookRequestDto requestDto);
 
     void updateBookFromDto(CreateBookRequestDto requestDto, @MappingTarget Book book);
+
+    BookDtoWithoutCategoryIds toDtoWithoutCategories(Book book);
+
+    @AfterMapping
+    default void setCategoryIds(@MappingTarget BookDto bookDto, Book book) {
+        List<Long> category = book.getCategories()
+                .stream()
+                .map(Category::getId)
+                .toList();
+        bookDto.setCategoryIds(category);
+    }
+
 }
